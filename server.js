954c57cf5c69b5f86b4cd577d36bb78ffe871e01
Nikil kumar <kumarnikil220@gmail.com>
2023-02-08 11:50:11 +0530

    endpoint to create a product

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const app = express();
-import { APP_PORT,DB_URL } from './config';
+import { APP_PORT, DB_URL } from './config';
 import routes from './routes';  //importing all routes 
 import errorHandler from './Middlewares/errorHandler.js'
 import mongoose from 'mongoose';
+import path from 'path';
 
 //Database connection
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,7 +14,14 @@ db.once('open', () => {
     console.log("Db connected...");
 });
 
+global.appRoot = path.resolve(__dirname);
 
+/* Abhi jaise json data ko receive karne ke liye hum built in express.json middleware
+  ka use karte hai thik aaise he hum multipart data ko receive karne ke liye hum
+  urlencoded builtin middleware ka use karte hai 
+*/
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 // '/api' is a default prefix of all requested routes.
